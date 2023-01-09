1de9d7f1288359519682c871933d1939677d5fad
HICHAM SALHI <hicham.salhi@usmba.ac.ma>
2023-01-09 10:20:23 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using SignalRWebpack.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapHub<ChatHub>("/hub");
+
+app.Run();
